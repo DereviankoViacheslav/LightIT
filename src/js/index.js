880fc38253336f4a97e1128f802bce64972c45dc
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2019-02-26 12:56:58 +0200

    git init

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import '../components/bootstrap/bootstrap.js';
+import '../sass/main.scss';
+import Img from '../img/bg/header_bg.png';
+
+// $(`.navbar`).html(`Text`);
+
+// function component() {
+//   let element = document.createElement('div');
+//   var btn = document.createElement('button');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
+//   element.classList.add('p-a-3');
+
+//   // Add the image to our existing div.
+//   var myImg = new Image();
+//   myImg.src = Img;
+//   element.appendChild(myImg);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+const path = require('path');
+const Webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  // mode: 'development',
+  entry: {
+    app: './src/js/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    overlay: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css',
+    }),
+    new Webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true
+          }
+        }],
+      },
+      canPrint: true
+    }),
+  ],
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpeg|svg|jpg|gif)$/i,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: './img/',
+              useRelativePath: true,
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: './fonts/',
+            useRelativePath: true,
+          },
+        }]
+      }
+    ]
+  }
+};
